e56fe517cc815bee64c942b2f27bd63112645784
Steve Zingman <N4IRS@users.noreply.github.com>
2026-07-14 17:40:19 -0400

    Convert USRPAudio.py to Python 3 and refactor code
    Updated script for Python 3 compatibility and improved audio handling.

--- USRPAudio.py
+++ USRPAudio.py
@@ -1,49 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 ###################################################################################
 # Copyright (C) 2014, 2015, 2016, 2017, 2018 N4IRR
-#
-# Permission to use, copy, modify, and/or distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND ISC DISCLAIMS ALL WARRANTIES WITH
-# REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-# AND FITNESS.  IN NO EVENT SHALL ISC BE LIABLE FOR ANY SPECIAL, DIRECT,
-# INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
-# LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
-# OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-# PERFORMANCE OF THIS SOFTWARE.
+# Py3 conversion + port fixes for EchoLink_Bridge
 ###################################################################################
+from time import time, sleep, localtime, strftime
+import socket, struct, _thread, sys, pyaudio
 
-from time import time, sleep, clock, localtime, strftime
-from random import randint
-import socket
-import struct
-import thread
-import shlex
-import pyaudio
-
+RX_PORT = 3211   # bridge sends audio here
+TX_PORT = 3210   # bridge reads audio from here
 ipAddress = "127.0.0.1"
 
 def rxAudioStream():
     global ipAddress
-    print('Start audio thread')
-
     FORMAT = pyaudio.paInt16
     CHUNK = 160
     CHANNELS = 1
     RATE = 8000
 
-    stream = p.open(format=FORMAT,
-                    channels = CHANNELS,
-                    rate = RATE,
-                    output = True,
-                    frames_per_buffer = CHUNK,
-                    )
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+                    output=True, frames_per_buffer=CHUNK)
 
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    udp.bind(("", 32001))
+    udp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    udp.bind(("", RX_PORT))
 
     lastKey = -1
     start_time = time()
@@ -55,41 +34,34 @@ def rxAudioStream():
         soundData, addr = udp.recvfrom(1024)
         if addr[0] != ipAddress:
             ipAddress = addr[0]
-        if (soundData[0:4] == 'USRP'):
-            eye = soundData[0:4]
-            seq, = struct.unpack(">i", soundData[4:8])
-            memory, = struct.unpack(">i", soundData[8:12])
-            keyup, = struct.unpack(">i", soundData[12:16])
-            talkgroup, = struct.unpack(">i", soundData[16:20])
-            type, = struct.unpack("i", soundData[20:24])
-            mpxid, = struct.unpack(">i", soundData[24:28])
-            reserved, = struct.unpack(">i", soundData[28:32])
+        if soundData[0:4] == b'USRP':
+            seq = struct.unpack(">i", soundData[4:8])[0]
+            memory = struct.unpack(">i", soundData[8:12])[0]
+            keyup = struct.unpack(">i", soundData[12:16])[0]
+            talkgroup = struct.unpack(">i", soundData[16:20])[0]
+            ptype = struct.unpack("i", soundData[20:24])[0]
+            mpxid = struct.unpack(">i", soundData[24:28])[0]
+            reserved = struct.unpack(">i", soundData[28:32])[0]
             audio = soundData[32:]
-            if (type == 0): # voice
-                audio = soundData[32:]
-                #print(eye, seq, memory, keyup, talkgroup, type, mpxid, reserved, audio, len(audio), len(soundData))
-                if (len(audio) == 320):
-                    stream.write(audio, 160)
-                if (keyup != lastKey):
-#                    print('key' if keyup else 'unkey')
+            if ptype == 0:  # voice
+                if len(audio) >= 320:
+                    stream.write(audio[:320], 160)
+                if keyup != lastKey:
                     if keyup:
                         start_time = time()
-                    if keyup == False:
-                        print '{} {} {} {} {} {} {:.2f}s'.format(
-                                                                    strftime(" %m/%d/%y", localtime(start_time)),
-                                                                    strftime("%H:%M:%S", localtime(start_time)),
-                                                                    call, rxslot, tg, loss, time() - start_time)
+                    else:
+                        print('{} {} {} {} {} {} {:.2f}s\r'.format(
+                            strftime(" %m/%d/%y", localtime(start_time)),
+                            strftime("%H:%M:%S", localtime(start_time)),
+                            call, rxslot, tg, loss, time() - start_time))
                     lastKey = keyup
-            if (type == 2): #metadata
-                audio = soundData[32:]
-                if ord(audio[0]) == 8:
-                    tg = (ord(audio[9]) << 16) + (ord(audio[10]) << 8) + ord(audio[11])
-                    rxslot = ord(audio[12]);
-                    call = audio[14:]
-
+            if ptype == 2:  # metadata
+                if audio[0] == 8:
+                    tg = (audio[9] << 16) + (audio[10] << 8) + audio[11]
+                    rxslot = audio[12]
+                    call = audio[14:].decode('ascii', errors='replace').rstrip('\x00')
         else:
             print(soundData, len(soundData))
-
     udp.close()
 
 def txAudioStream():
@@ -98,12 +70,18 @@ def txAudioStream():
     CHANNELS = 1
     RATE = 8000
 
+#    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE,
+#                    input=True, frames_per_buffer=CHUNK)
+
     stream = p.open(format=FORMAT,
                     channels = CHANNELS,
                     rate = RATE,
                     input = True,
                     frames_per_buffer = CHUNK,
+                    input_device_index=None
                     )
+
+
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     lastPtt = ptt
     seq = 0
@@ -111,53 +89,44 @@ def txAudioStream():
         try:
             audio = stream.read(160, exception_on_overflow=False)
             if ptt != lastPtt:
-                usrp = 'USRP' + struct.pack('>iiiiiii',seq, 0, ptt, 0, 0, 0, 0)
-                udp.sendto(usrp, (ipAddress, 34001))
-                seq = seq + 1
-                print 'PTT: {}'.format(ptt)
+                usrp = b'USRP' + struct.pack('>iiiiiii', seq, 0, int(ptt), 0, 0, 0, 0)
+                udp.sendto(usrp, (ipAddress, TX_PORT))
+                seq += 1
+                print('PTT: {}'.format(ptt))
             lastPtt = ptt
             if ptt:
-                usrp = 'USRP' + struct.pack('>iiiiiii',seq, 0, ptt, 0, 0, 0, 0) + audio
-                udp.sendto(usrp, (ipAddress, 34001))
-                print 'transmitting'
-                seq = seq + 1
-        except:
-            print("overflow")
+                usrp = b'USRP' + struct.pack('>iiiiiii', seq, 0, 1, 0, 0, 0, 0) + audio
+                udp.sendto(usrp, (ipAddress, TX_PORT))
+                seq += 1
+        except OSError:
+            pass
 
 def _find_getch():
     try:
-        import termios
+        import termios, tty
     except ImportError:
-        # Non-POSIX. Return msvcrt's (Windows') getch.
         import msvcrt
         return msvcrt.getch
-
-    # POSIX system. Create and return a getch that manipulates the tty.
-    import sys, tty
+    import sys as _sys
     def _getch():
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
+        fd = _sys.stdin.fileno()
+        old = termios.tcgetattr(fd)
         try:
             tty.setraw(fd)
-            ch = sys.stdin.read(1)
+            return _sys.stdin.read(1)
         finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
+            termios.tcsetattr(fd, termios.TCSADRAIN, old)
     return _getch
 
-ptt = False     # toggle this to transmit (left up to you)
+ptt = False
 
 p = pyaudio.PyAudio()
-thread.start_new_thread( rxAudioStream, () )
-thread.start_new_thread( txAudioStream, () )
+_thread.start_new_thread(rxAudioStream, ())
+_thread.start_new_thread(txAudioStream, ())
 
-##root.mainloop()
 getch = _find_getch()
 while True:
     ch = getch()
-    if ord(ch) == 3:
-        exit(0)
-    ptt = ~ptt
-
-
+    if ch == '\x03':
+        sys.exit(0)
+    ptt = not ptt
