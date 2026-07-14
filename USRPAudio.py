b28d59172a953ab6374b1ea58df4d3fc32892967
Steve Zingman <N4IRS@users.noreply.github.com>
2026-07-14 17:45:37 -0400

    Revise copyright and license in USRPAudio.py
    Updated copyright information and license details.

--- USRPAudio.py
+++ USRPAudio.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 ###################################################################################
-# Copyright (C) 2014, 2015, 2016, 2017, 2018 N4IRR
+# DVSwitch-USRP_Audio
+# Copyright (C) 2014 - 2026 N4IRR
+# Copyright (C) 2026 by DVSwitch KaT
 # Py3 conversion + port fixes for EchoLink_Bridge
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 ###################################################################################
 from time import time, sleep, localtime, strftime
 import socket, struct, _thread, sys, pyaudio
